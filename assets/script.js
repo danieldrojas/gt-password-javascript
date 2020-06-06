7e75b49356aa8c6672c8eacd70af3bde6d6a7835
Daniel Rojas <daniel.drojas@outlook.com>
2020-06-06 12:14:55 -0400

    added pics on README

--- assets/script.js
+++ assets/script.js
@@ -14,94 +14,108 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
- function generatePassword() {
+function generatePassword() {
 
-  //   Prompt user to create password 
+  //   Prompt user to create password `
 
-  alert("crete password?");
+  alert("Create password?");
   var passwordLength = prompt("How many characters would you like to include?");
+ 
+  if(!(passwordLength)) {
 
-  //Loop to validate input 
-  while (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("Invalid password length, Try again");
+  
+    var password = "You did not create a password";
+    return password;
+
+
+
+     
+  }
+  else {
+
+  
+   
+    //Loop to validate input 
+    while (passwordLength < 8 || passwordLength > 128) {
+      passwordLength = prompt("Invalid password length, Try again");
+    }
+
+
+    //Variables 
+
+    var okSpecialChar = confirm("Click Ok to confirm special characters");
+
+    var okLowerCase = confirm("Click Ok to confirm including lowercase character");
+
+    var okUpperCase = confirm("Click Ok to confirm including uppercase character");
+
+    var okNumbers = confirm("Click Ok to confirm including numbers");
+
+    password = "";
+
+    var allSpecialChar = "!#$%&'“,()*+,-./:;<=>?@[]^_`{|}~";
+
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+    //Outsider loop to fill password length
+    while (password.length < passwordLength) {
+
+      if (okSpecialChar && password.length < passwordLength) {
+
+        var index = Math.floor(Math.random() * (allSpecialChar.length + 1)) - 1;
+
+        password = password + allSpecialChar.charAt(index);
+
+        console.log(password);
+      }
+
+      if (okLowerCase && password.length < passwordLength) {
+
+        var index = Math.floor(Math.random() * alphabet.length) - 1;
+
+        password = password + alphabet[index];
+
+        console.log(password);
+      }
+
+
+      if (okUpperCase && password.length < passwordLength) {
+
+        var index = Math.floor(Math.random() * alphabet.length)
+
+        password = password + alphabet[index].toUpperCase();
+
+        console.log(password);
+
+      }
+
+      console.log(password);
+
+      if (okNumbers && password.length < passwordLength) {
+
+        console.log(password);
+
+        var index = Math.floor(Math.random() * numbers.length);
+
+        console.log(index);
+
+        password = password + numbers[index];
+
+        console.log(password);
+
+      }
+
+
+
+    }
+    return password;
+
+
   }
 
-
-  //Variables 
-
-  var okSpecialChar = confirm("Click Ok to confirm special characters");
-
-  var okLowerCase = confirm("Click Ok to confirm including lowercase character");
-
-  var okUpperCase = confirm("Click Ok to confirm including uppercase character");
-
-  var okNumbers = confirm("Click Ok to confirm including numbers");
-
-  var password = "";
-
-  var allSpecialChar = "!#$%&'“,()*+,-./:;<=>?@[]^_`{|}~";
-
-  var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
-  //Outsider loop to fill password length
-  while (password.length < passwordLength) {
-
-    if (okSpecialChar && password.length < passwordLength) {
-
-      var index = Math.floor(Math.random() * (allSpecialChar.length + 1)) - 1;
-
-      password = password + allSpecialChar.charAt(index);
-
-      console.log(password);
-    }
-
-    if (okLowerCase && password.length < passwordLength) {
-
-      var index = Math.floor(Math.random() * alphabet.length) - 1;
-
-      password = password + alphabet[index];
-
-      console.log(password);
-    }
-
-
-    if (okUpperCase && password.length < passwordLength) {
-
-      var index = Math.floor(Math.random() * alphabet.length)
-
-      password = password + alphabet[index].toUpperCase();
-
-      console.log(password);
-
-    }
-
-    console.log(password);
-
-    if (okNumbers && password.length < passwordLength) {
-
-      console.log(password);
-
-      var index = Math.floor(Math.random() * numbers.length);
-
-      console.log(index);
-
-      password = password + numbers[index];
-
-      console.log(password);
-
-    }
-
-
-
 }
-  return password;
-
-
-}
-
-
 
 
